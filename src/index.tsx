d9fc7a4dae55e166a91348a14ef38d90e9bcee88
Michael Lawrie <michael@michaellawrie.com>
2016-05-11 21:45:09 -0500

    Set up webpack

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <div>
+    <h1>Hello</h1>
+  </div>,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const appEntries = ['./src/index.tsx']
+  .concat(process.env.NODE_ENV === 'development' ? 'webpack-hot-middleware/client?reload=true' : []);
+
+module.exports = {
+  entry: {
+    app: appEntries,
+  },
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    sourceMapFilename: '[name].js.map'
+  },
+  
+  plugins: [new HtmlWebpackPlugin({template: './src/index.html'})],
+
+  devtool: 'source-map',
+
+  resolve: {
+    extensions: ['', '.webpack.js', '.web.js', '.tsx', '.ts', '.js'],
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts',
+        exclude: /(node_modules|test-utils|\.test\.ts$)/
+      }
+    ],
+  }
+};
