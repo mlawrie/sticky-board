a068b6dcd6b16209950a6aa4748e191450db58fb
Michael Lawrie <michael@michaellawrie.com>
2016-05-14 16:33:50 -0500

    Implement resilient sticky sorting

--- src/stickyReducers.ts
+++ src/stickyReducers.ts
@@ -1,22 +1,21 @@
 import {isType, updateStickyAction, createStickyAction, moveStickyToTop, Action} from './actions'
 import {Sticky, modifySticky} from './sticky'
 import combine from './combine'
-import uuid = require('node-uuid')
+import makeUuid = require('node-uuid')
 import Immutable = require('immutable')
 
 type Stickies = Immutable.Map<string, Sticky>
 
-const demoteZ = (sticky: Sticky) => modifySticky(sticky, {z: sticky.z - 1})
-    
 export interface StickiesState {
   readonly stickies: Stickies 
 }
 
 export const stickiesReducers = (state: Stickies = Immutable.Map<string, Sticky>(), action: Action<any>): Stickies => {
+  const getUuuid = (sticky:Sticky) => state.keyOf(sticky)
   
   if (isType(action, createStickyAction)) {
-    const foregroundSticky = combine(action.payload, {z: state.size + 1}) 
-    return state.set(uuid.v1(), foregroundSticky)
+    const foregroundSticky = combine(action.payload, {z: state.size + 1})
+    return state.set(makeUuid.v1(), foregroundSticky)
   }
    
   if (isType(action, updateStickyAction)) {
@@ -27,8 +26,15 @@ export const stickiesReducers = (state: Stickies = Immutable.Map<string, Sticky>
   
   if (isType(action, moveStickyToTop)) {
     const existingSticky = state.get(action.payload.uuid)
-    const foregroundSticky = modifySticky(existingSticky, {z: state.size + 1})
-    return state.map(demoteZ).toMap().set(action.payload.uuid, foregroundSticky)
+    const foregroundSticky = modifySticky(existingSticky, {z: state.size})
+   
+    const otherUuids = state.keySeq().toArray().filter((uuid) => uuid !== action.payload.uuid)
+    const sorted = otherUuids.map((uuid) => ({uuid, sticky: state.get(uuid)}))
+        .sort((a, b) => a.sticky.z - b.sticky.z)
+   
+    const assignZ = (sticky:Sticky) => modifySticky(sticky, {z: 1 + sorted.findIndex((el) => el.uuid === getUuuid(sticky))})
+    
+    return state.map(assignZ).toMap().set(action.payload.uuid, foregroundSticky)
   }
   
   return state
