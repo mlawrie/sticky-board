c868b7c1d913aa06c19f6638e27411404d72d900
Michael Lawrie <michael@michaellawrie.com>
2016-06-05 20:02:45 -0500

    Remove unused file

--- server/models/sticky.js
+++
@@ -1,9 +0,0 @@
-const db = require('../database')
-const Board = require('./board')
-
-const Sticky = db.Model.extend({
-  tableName: 'stickies',
-  board: () => this.belongsTo(Board)
-})
-
-module.exports = Sticky
