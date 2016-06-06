cb88cb15622c1090f1a2db3d53c89abd57707f3c
Michael Lawrie <michael@michaellawrie.com>
2016-06-05 22:25:09 -0500

    Serve client from node

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,7 @@
 import * as express from 'express'
-
-import { boardRouter } from './boards/boardRouter'
-
 import * as bodyParser from 'body-parser'
+import { boardRouter } from './boards/boardRouter'
+import * as  path from 'path'
 
 export const app = express()
 app.use(bodyParser.json())
@@ -10,3 +9,22 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(boardRouter)
 
+app.use(express.static(path.join(__dirname, '../public')))
+
+const clientRouter = express.Router()
+
+clientRouter.get('/boards/:name/:urlToken', (req:express.Request, res:express.Response) => {
+  res.send(200, `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+      </head>
+      <body>
+        <div id="app"></div>
+        <script type="text/javascript" src="/app.js"></script>
+      </body>
+    </html>
+  `)
+})
+
+app.use(clientRouter)
